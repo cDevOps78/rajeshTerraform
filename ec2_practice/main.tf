ab066ad1219427a18f09ab1af6611ecd4e6fe861
chaitu1 <chaitu143@gmail.com>
2024-03-31 22:40:01 +0530

    rajesh devops first code

--- ec2_practice/main.tf
+++ ec2_practice/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "frontend" {
   for_each      = var.components
 
   ami           = "ami-009e46eef82e25fef"
-  instance_type = lookup(each.value, "instance_type", "t3.medium" )
+  # instance_type = lookup(each.value, "instance_type", "t3.medium" )
+  instance_type = var.components[each.key].instance_type
   tags          = lookup(lookup(var.components, each.key, "nokey"),"tags",{Name: "Default", Project: "OtherProject"})
 }
--- ec2_practice/variables.tf
+++ ec2_practice/variables.tf
@@ -2,17 +2,17 @@ variable "components" {
   default = {
     frontend = {
       instance_type = "t2.micro"
-#      tags          = {
-#        Name    = "frontend"
-#        project = "expense"
-#      }
+      tags          = {
+        Name    = "frontend"
+        project = "expense"
+      }
     }
     backend = {
       instance_type = "t2.small"
-#      tags          = {
-#        Name    = "backend"
-#        project = "expense"
-#      }
+      tags          = {
+        Name    = "backend"
+        project = "expense"
+      }
     }
   }
 }
