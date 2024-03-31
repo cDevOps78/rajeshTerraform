0f22c7c7f0ad8d3a631f1dd97330bbf30aa7aeeb
chaitu1 <chaitu143@gmail.com>
2024-03-31 23:13:31 +0530

    rajesh devops first code

--- ec2_practice/main.tf
+++ ec2_practice/main.tf
@@ -2,18 +2,42 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "frontend" {
-  for_each      = var.components
-
-  ami           = "ami-009e46eef82e25fef"
-  # instance_type = lookup(each.value, "instance_type", "t3.medium" )
-  instance_type = each.value["instance_type"]
-
-  # instance_type = var.components[each.key].instance_type
-  # tags          = lookup(lookup(var.components, each.key, "nokey"),"tags",{Name: "Default", Project: "OtherProject"})
-  tags            = var.components[each.key].tags
-}
+#resource "aws_instance" "frontend" {
+#  for_each      = var.components
+#
+#  ami           = "ami-009e46eef82e25fef"
+#  # instance_type = lookup(each.value, "instance_type", "t3.medium" )
+#  instance_type = each.value["instance_type"]
+#
+#  # instance_type = var.components[each.key].instance_type
+#  # tags          = lookup(lookup(var.components, each.key, "nokey"),"tags",{Name: "Default", Project: "OtherProject"})
+#  tags            = var.components[each.key].tags
+#}
 
 #output "name" {
 #  value = var.components["frontend"]["tags"]["project"]
 #}
+
+data "aws_security_group" "default" {
+  for_each = var.components
+
+  name = "${each.key}_group"
+  description: "This is for ${each.key}_group"
+
+  ingress  {
+    form_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress  {
+    form_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${each.key}"
+  }
+}
