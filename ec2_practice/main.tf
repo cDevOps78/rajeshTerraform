c21682a86f1c4357ad5b5aea39504052a62fcfb6
chaitu1 <chaitu143@gmail.com>
2024-03-31 22:56:26 +0530

    rajesh devops first code

--- ec2_practice/main.tf
+++ ec2_practice/main.tf
@@ -2,16 +2,18 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-#resource "aws_instance" "frontend" {
-#  for_each      = var.components
-#
-#  ami           = "ami-009e46eef82e25fef"
-#  # instance_type = lookup(each.value, "instance_type", "t3.medium" )
-#  # instance_type = var.components[each.key].instance_type
-#   instance_type = each.value["instance_type"]
-#  tags          = lookup(lookup(var.components, each.key, "nokey"),"tags",{Name: "Default", Project: "OtherProject"})
-#}
+resource "aws_instance" "frontend" {
+  for_each      = var.components
 
-output "name" {
-  value = var.components["frontend"]["tags"]["project"]
+  ami           = "ami-009e46eef82e25fef"
+  # instance_type = lookup(each.value, "instance_type", "t3.medium" )
+  instance_type = each.value["instance_type"]
+
+  # instance_type = var.components[each.key].instance_type
+  # tags          = lookup(lookup(var.components, each.key, "nokey"),"tags",{Name: "Default", Project: "OtherProject"})
+  tags            = var.components[each.key].tags
 }
+
+#output "name" {
+#  value = var.components["frontend"]["tags"]["project"]
+#}
