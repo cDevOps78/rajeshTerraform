77fa50dd98dfe4728b20be3309aa51b97ae4d6be
chaitu1 <chaitu143@gmail.com>
2024-04-10 20:21:05 +0530

    learning file-provisioner provisioners

--- provisioners/file-provisioner/main.tf
+++ provisioners/file-provisioner/main.tf
@@ -8,7 +8,8 @@ resource "aws_instance" "prod_server" {
   provisioner "remote-exec" {
     on_failure = continue
     inline = [
-      "sudo dnf install nginx -y"
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
     ]
   }
   connection {
@@ -18,5 +19,4 @@ resource "aws_instance" "prod_server" {
     host     = self.private_ip
     port     = 22
   }
-
 }
