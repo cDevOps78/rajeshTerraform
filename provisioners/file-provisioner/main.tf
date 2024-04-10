b85caf5c39abe4008a3a8532e59449f6fdaf1cae
chaitu1 <chaitu143@gmail.com>
2024-04-10 18:28:13 +0530

    learning file-provisioner provisioners

--- provisioners/file-provisioner/main.tf
+++ provisioners/file-provisioner/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "prod_server" {
   ami           = data.aws_ami_ids.devops-practice.ids[0]
   instance_type = "t2.micro"
+  tags          = {
+    Name = "prod-server"
+  }
 
   connection {
     type     = "ssh"
