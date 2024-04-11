2f1c95ef799efa721f4e348800b63f99ddb98933
chaitu1 <chaitu143@gmail.com>
2024-04-11 15:24:59 +0530

    learning file-provisioner provisioners

--- provisioners/remote-provisioner/main.tf
+++ provisioners/remote-provisioner/main.tf
@@ -1,29 +1,29 @@
-#resource "aws_instance" "test" {
-#  ami           = "ami-090252cbe067a9e58"
-#  instance_type = "t2.micro"
-#  vpc_security_group_ids = ["sg-0a88820d7b4d3ff2a"]
-#}
-#
-#resource "null_resource" "test1" {
-#  provisioner "remote-exec" {
-#    connection {
-#      type = "ssh"
-#      user = ""
-#      password = ""
-#      host = aws_instance.test.private_ip
-#    }
-#    inline = [
-#         "sudo yum install nginx -y"
-#    ]
-#  }
-#}
+resource "aws_instance" "test" {
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0a88820d7b4d3ff2a"]
+}
+
+resource "null_resource" "test1" {
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = data.vault_kv_secret.secret_data.data["ansible_user"]
+      password = data.vault_kv_secret.secret_data.data["ansible_password"]
+      host = aws_instance.test.private_ip
+    }
+    inline = [
+         "sudo yum install nginx -y"
+    ]
+  }
+}
 
 
 data "vault_kv_secret" "secret_data" {
   path = "common/ssh"
 }
 
-resource "local_file" "foo" {
-  content  = data.vault_kv_secret.secret_data.data["ansible_user"]
-  filename = "/tmp/hello.secrets1"
-}
+#resource "local_file" "foo" {
+#  content  = data.vault_kv_secret.secret_data.data["ansible_user"]
+#  filename = "/tmp/hello.secrets1"
+#}
