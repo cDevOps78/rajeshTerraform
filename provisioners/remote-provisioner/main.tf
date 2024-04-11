2094bafdf9cd97e43f088e75bdffb3e8415af593
chaitu1 <chaitu143@gmail.com>
2024-04-11 21:19:26 +0530

    learning file-provisioner provisioners

--- provisioners/remote-provisioner/main.tf
+++ provisioners/remote-provisioner/main.tf
@@ -47,3 +47,9 @@ resource "local_file" "foo" {
   content  = "ansible_user: ${data.vault_kv_secret.secret_data.data["ansible_user"]}\nansible_password: ${data.vault_kv_secret.secret_data.data["ansible_password"]}"
   filename = "./secrets.yaml"
 }
+
+resource "null_resource" "ansible_playbook" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -e '@secrets.yaml' playbook.yaml"
+  }
+}
