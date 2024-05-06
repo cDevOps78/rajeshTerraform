ca1bf6bfed47906778f4f97decbeb3e2ec58fa63
chaitu1 <chaitu143@gmail.com>
2024-05-06 13:44:39 +0530

    variables day 1

---
+++ variables01/varday1/main.tf
@@ -0,0 +1,7 @@
+output "name" {
+  value = var.name
+}
+
+output "name-length" {
+    value = var.length 
+}
---
+++ variables01/varday1/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    default = "chaithanya/ram"
+}
+
+variable "splits" {
+  default = split("/", var.name)[0]
+}
+
+variable "length" {
+  default = length(var.name)
+}
