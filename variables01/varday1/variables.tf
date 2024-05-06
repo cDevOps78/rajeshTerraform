9c465fc95f1ff728d1be3e50521820fa88edce49
chaitu1 <chaitu143@gmail.com>
2024-05-06 14:08:44 +0530

    push

--- variables01/varday1/variables.tf
+++ variables01/varday1/variables.tf
@@ -1,5 +1,9 @@
 variable "name" {
-    default = "chaithanya-ram"
+    default = "chaithanya-ram-ram1"
+    validation {
+      condition = length(split("-".var.name)) <= 2
+      error_message = "The length of name should be less than 2" 
+    }
    # sensitive = true
 }
 
