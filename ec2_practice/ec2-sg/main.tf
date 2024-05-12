a7da7f736e95f3118f5d2958dd4b7359bff17844
chaitu1 <chaitu143@gmail.com>
2024-05-12 20:48:16 +0530

    working with sg resource

---
+++ ec2_practice/ec2-sg/main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "sample" {
+  ami           = "ami-04ff98ccbfa41c9ad"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sample-sg.id]
+}
+
+resource "aws_security_group" "sample-sg" {
+  name = "sample-sg"
+  vpc_id = "vpc-0dfa2ee2180ae8e1a"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sample-sg"
+  }
+}
