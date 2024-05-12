b8b88e188ad9088ccee85eb34844b0261955a8cf
chaitu1 <chaitu143@gmail.com>
2024-05-12 21:36:40 +0530

    multiple provisioners

--- ec2_practice/ec2-sg/main.tf
+++ ec2_practice/ec2-sg/main.tf
@@ -1,42 +1,32 @@
-#resource "aws_instance" "sample" {
-#  ami           = "ami-04ff98ccbfa41c9ad"
-#  instance_type = "t2.micro"
-#  vpc_security_group_ids = [aws_security_group.sample-sg.id]
-#}
-#
-#resource "aws_security_group" "sample-sg" {
-#  name = "sample-sg2"
-#  vpc_id = "vpc-0dfa2ee2180ae8e1a"
-#
-#  timeouts {
-#    delete = "1m"
-#  }
-#
-#  ingress {
-#    from_port = 22
-#    to_port   = 22
-#    protocol = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  egress {
-#    from_port = 0
-#    to_port   = 0
-#    protocol = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  tags = {
-#    Name = "sample-sg"
-#  }
-#}
+resource "aws_instance" "sample" {
+  ami           = "ami-04ff98ccbfa41c9ad"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sample-sg.id]
+}
 
-###### on failure ##########
+resource "aws_security_group" "sample-sg" {
+  name = "sample-sg2"
+  vpc_id = "vpc-0dfa2ee2180ae8e1a"
 
-resource "null_resource" "sample3" {
-  provisioner "local-exec" {
-  command = "echo This is provisioner1"
+  timeouts {
+    delete = "1m"
   }
-  provisioner "local-exec" {
-    command = "echo This is provisioner2"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sample-sg"
   }
 }
+
