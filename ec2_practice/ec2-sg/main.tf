7e13069c1cf6aa05c9808a404c4ef753ac7e5ff4
chaitu1 <chaitu143@gmail.com>
2024-05-12 21:22:22 +0530

    using time out

--- ec2_practice/ec2-sg/main.tf
+++ ec2_practice/ec2-sg/main.tf
@@ -1,31 +1,39 @@
-resource "aws_instance" "sample" {
-  ami           = "ami-04ff98ccbfa41c9ad"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sample-sg.id]
-}
+#resource "aws_instance" "sample" {
+#  ami           = "ami-04ff98ccbfa41c9ad"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.sample-sg.id]
+#}
+#
+#resource "aws_security_group" "sample-sg" {
+#  name = "sample-sg2"
+#  vpc_id = "vpc-0dfa2ee2180ae8e1a"
+#
+#  timeouts {
+#    delete = "1m"
+#  }
+#
+#  ingress {
+#    from_port = 22
+#    to_port   = 22
+#    protocol = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port = 0
+#    to_port   = 0
+#    protocol = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags = {
+#    Name = "sample-sg"
+#  }
+#}
 
-resource "aws_security_group" "sample-sg" {
-  name = "sample-sg2"
-  vpc_id = "vpc-0dfa2ee2180ae8e1a"
+###### on failure ##########
 
-  timeouts {
-    delete = "1m"
-  }
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sample-sg"
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo This is chaithanya"
   }
 }
