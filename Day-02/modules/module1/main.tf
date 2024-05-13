0668bc117b44b56ea07a24ae241a913a476d331b
chaitu1 <chaitu143@gmail.com>
2024-05-13 11:18:16 +0530

    only cmd is used

--- Day-02/modules/module1/main.tf
+++ Day-02/modules/module1/main.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "sample" {
+resource "null_resource" "sample2" {
   provisioner "local-exec" {
     command = "echo This is resource -1 - ${var.name}"
   }
 }
 
-resource "null_resource" "sample1" {
+resource "null_resource" "sample3" {
   provisioner "local-exec" {
     command = "echo This is resource -2 ${var.name}"
   }
