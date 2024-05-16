0168bd215f9bb5f38df2b3135b39fa17be4e9f32
chaitu1 <chaitu143@gmail.com>
2024-05-16 13:36:21 +0530

    kubernetes learning probes

--- Day-02/modules/module2/main.tf
+++ Day-02/modules/module2/main.tf
@@ -1,5 +1,7 @@
-output "return-module1" {
-  value = var.passing
+resource "null_resource" "top" {
+  provisioner "local-exec" {
+    command = "echo This module from1 vaule ${var.passing}"
+  }
 }
 
 variable "passing" {}
